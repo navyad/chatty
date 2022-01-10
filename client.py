2ec7756672ee05356ab0319a6f61f243667df56d
navyad <navyad.pro@gmail.com>
2022-01-10 18:07:19 +0530

    decode msg

--- client.py
+++ client.py
@@ -6,6 +6,6 @@ socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_obj.connect(SERVER_HOST_PORT)
 
 while True:
-    client_msg = input("you: ")
-    socket_obj.send(b"hi from client")
-    server_message = socket_obj.recv(1024)
+    input_msg = input("you: ").encode()
+    assert isinstance(input_msg, bytes)
+    socket_obj.send(input_msg)
--- server.py
+++ server.py
@@ -4,10 +4,10 @@ HOST_PORT = ("127.0.0.1", 1234)
 
 socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_obj.bind(HOST_PORT)
-socket_obj.listen(3)
+socket_obj.listen()
+client_soc, addr = socket_obj.accept()
 
 while True:
-    client_soc, addr = socket_obj.accept()
     msg = client_soc.recv(1024).decode()
-    print(msg)
-    client_soc.send(b"from server")
+    fmt_msg = f"msg received: {msg}"
+    print(fmt_msg)
