216bbde4f5a8bd45025e356db5c718ec405a23a9
navyad <navyad.pro@gmail.com>
2022-01-10 16:17:38 +0530

    removing printf

--- client.py
+++ client.py
@@ -3,11 +3,9 @@ import socket
 SERVER_HOST_PORT = ("127.0.0.1", 1234)
 
 socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("connected with server....")
 socket_obj.connect(SERVER_HOST_PORT)
 
 while True:
     client_msg = input("you: ")
     socket_obj.send(b"hi from client")
-    server_message = socket_obj.recv(10)
-    print(f"{server_message}")
+    server_message = socket_obj.recv(1024)
--- server.py
+++ server.py
@@ -5,7 +5,6 @@ HOST_PORT = ("127.0.0.1", 1234)
 socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_obj.bind(HOST_PORT)
 socket_obj.listen(3)
-print(f"server running at {HOST_PORT}")
 
 while True:
     client_soc, addr = socket_obj.accept()
