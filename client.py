659007f583137cdcb35f2928e45131e6067d6817
navyad <navyad.pro@gmail.com>
2022-01-12 18:57:55 +0530

    refact #2

--- client.py
+++ client.py
@@ -1,10 +1,21 @@
 import socket
+from typing import Tuple
+from constants import SERVER_HOST_PORT
 
-SERVER_HOST_PORT = ("127.0.0.1", 1234)
-socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_obj.connect(SERVER_HOST_PORT)
 
-while True:
-    input_msg = input("you: ").encode()
-    assert isinstance(input_msg, bytes)
-    socket_obj.send(input_msg)
+def connect_server(address: Tuple[str, str]) -> socket.socket:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.connect(address)
+    return server_socket
+
+
+def send_input(server_socket: socket.socket):
+    while True:
+        input_msg = input("you: ").encode()
+        assert isinstance(input_msg, bytes)
+        server_socket.send(input_msg)
+
+
+if __name__ == "__main__":
+    server_socket = connect_server(address=SERVER_HOST_PORT)
+    send_input(server_socket=server_socket)
