7a9ff7c3ef3666a8614509759c76e80f5bd3c829
navyad <navyad.pro@gmail.com>
2022-01-12 18:45:26 +0530

    minor refact #1

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
 import socket
+from typing import Tuple
+from constants import SERVER_HOST_PORT
 
-HOST_PORT = ("127.0.0.1", 1234)
 
-socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_obj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_obj.bind(HOST_PORT)
-socket_obj.listen()
-client_soc, addr = socket_obj.accept()
+def setup_server(address: Tuple[str, str], backlog: int = 100) -> Tuple[socket.socket, str]:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(address)
+    server_socket.listen(backlog)
+    return server_socket.accept()
 
-while True:
-    msg = client_soc.recv(1024).decode()
-    fmt_msg = f"msg received: {msg}"
-    print(fmt_msg)
+
+def process_client(client_socket: socket.socket) -> None:
+    while True:
+        msg = client_socket.recv(1024).decode()
+        fmt_msg = f"msg received: {msg}"
+        print(fmt_msg)
+
+
+if __name__ == "__main__":
+    client_socket, client_address = setup_server(address=SERVER_HOST_PORT)
+    process_client(client_socket=client_socket)
