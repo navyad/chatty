92b0eea7f96f9896e13902d4c51a7f3737aaae34
navyad <navyad.pro@gmail.com>
2022-02-21 07:13:53 +0530

    var name

--- client.py
+++ client.py
@@ -4,18 +4,18 @@ from constants import SERVER_HOST_PORT
 
 
 def connect_server(address: Tuple[str, str]) -> socket.socket:
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.connect(address)
-    return server_socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(address)
+    return client_socket
 
 
-def send_input(server_socket: socket.socket):
+def send_input(client_socket: socket.socket):
     while True:
         input_msg = input("you: ").encode()
-        assert isinstance(input_msg, bytes)
-        server_socket.send(input_msg)
+        client_socket.send(input_msg)
+    client_socket.close()
 
 
 if __name__ == "__main__":
-    server_socket = connect_server(address=SERVER_HOST_PORT)
-    send_input(server_socket=server_socket)
+    client_socket = connect_server(address=SERVER_HOST_PORT)
+    send_input(client_socket=client_socket)
